a64efd46fa2c04149c3a21c5b7ba6f20dd3d7c71
Katie Ng <katieng@Katie-MBP.lan>
2017-10-28 12:47:08 +1100

    feat: render the units fetched

--- src/containers/unit_list.js
+++ src/containers/unit_list.js
@@ -3,15 +3,36 @@ import { connect } from 'react-redux'
 
 import UnitListItem from '../components/unit_list_item'
 class UnitList extends Component {
+  constructor(props){
+    super(props)
+    this.renderUnits.bind(this)
+    this.renderUnit.bind(this)
+  }
+
+  renderUnit(unitData){
+    return(
+      <UnitListItem />
+    )
+  }
+
+
+  renderUnits(unitlist){
+    var maxLen=5
+    if (unitlist.data){
+      maxLen = (unitlist.data.length < maxLen) ? unitlist.data.length : maxLen;
+      return(
+        <div>
+          {unitlist.data.splice(0,maxLen).map(this.renderUnit)}
+        </div>
+      )
+    }
+    return
+  }
 
   render(){
     return (
       <ul className="mdc-list my-bordered-list"id="unit_list">
-        <UnitListItem />
-        <li>
-          Test
-          {console.log(this.props.units)}
-        </li>
+        {this.renderUnits(this.props.units)}
       </ul>
 
     );
